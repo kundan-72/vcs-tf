3a02f9af47ddafe6def91b65511497947aa56fb8
Kundan Doiphode <kundan7kundan@gmail.com>
2022-02-10 15:27:28 +0530

    add bucket

--- variables.tf
+++ variables.tf
@@ -24,4 +24,4 @@ variable "public_key" {
   # default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5a4eprJHJ0nmNjeI2mbxiFDdbR8MMp6R0/ptEgFdL65INs+DUs7+3RFcPvk0ybiY8exRH/fQsVbKTGfjnlVTt4NnigdvJmAS6ty+5+UVsSGBCQeg92perB955Py2NBeIMZ7E/4RphA8wndmmsrDv3FLMvO+mTQa4W3Dc0q5yoh9LRm/kdDgtHfIdmbEppUsKtx7zVRfViLoQG8fIw5qb3aHO/vF8D/4yfyB1pSD9xFGvSe/YWCboyV1aldmznBxTg2DbhOvgQCYRhTAjb/A56INw74QUky7YfCFtLJeJXZ5VMvd4Gwh9sd8N0U9Jyhp92Gnec/YkdOFJ3GVpR47P5 hcltech/kundan.doiphode@LP-5CG125CZ4P"
 }
 
-# hello
+# hello my nam y is kundan
