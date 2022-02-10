d9a4ee231de8e9a82c7bf6c38112bbf6dd14a47e
Kundan Doiphode <kundan.doiphode@hcl.com>
2022-02-10 13:23:42 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+  }
+    cloud {
+    	organization = "KundanCorp"
+
+    	workspaces {
+      	name = "vcs-tf"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket
+}
+module "apache" {
+  source          = "ExamProCo/apache-example/aws"
+  version         = "1.1.0"
+  vpc_id          = var.vpc_id
+  my_ip_with_cidr = var.my_ip_with_cidr
+  public_key      = var.public_key
+  instance_type   = var.instance_type
+  server_name     = var.server_name
+
+}
+
+output "public_ip" {
+  value = module.apache.public_ip
+}
+
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "my_ip_with_cidr" {
+  type = string
+}
+
+variable "bucket" {
+  type = string
+  default = "423749327492"
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "server_name" {
+  type = string
+}
+
+variable "public_key" {
+  type = string
+  # default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5a4eprJHJ0nmNjeI2mbxiFDdbR8MMp6R0/ptEgFdL65INs+DUs7+3RFcPvk0ybiY8exRH/fQsVbKTGfjnlVTt4NnigdvJmAS6ty+5+UVsSGBCQeg92perB955Py2NBeIMZ7E/4RphA8wndmmsrDv3FLMvO+mTQa4W3Dc0q5yoh9LRm/kdDgtHfIdmbEppUsKtx7zVRfViLoQG8fIw5qb3aHO/vF8D/4yfyB1pSD9xFGvSe/YWCboyV1aldmznBxTg2DbhOvgQCYRhTAjb/A56INw74QUky7YfCFtLJeJXZ5VMvd4Gwh9sd8N0U9Jyhp92Gnec/YkdOFJ3GVpR47P5 hcltech/kundan.doiphode@LP-5CG125CZ4P"
+}
+
+# public_key    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5a4eprJHJ0nmNjeI2mbxiFDdbR8MMp6R0/ptEgFdL65INs+DUs7+3RFcPvk0ybiY8exRH/fQsVbKTGfjnlVTt4NnigdvJmAS6ty+5+UVsSGBCQeg92perB955Py2NBeIMZ7E/4RphA8wndmmsrDv3FLMvO+mTQa4W3Dc0q5yoh9LRm/kdDgtHfIdmbEppUsKtx7zVRfViLoQG8fIw5qb3aHO/vF8D/4yfyB1pSD9xFGvSe/YWCboyV1aldmznBxTg2DbhOvgQCYRhTAjb/A56INw74QUky7YfCFtLJeJXZ5VMvd4Gwh9sd8N0U9Jyhp92Gnec/YkdOFJ3GVpR47P5 hcltech/kundan.doiphode@LP-5CG125CZ4P"
+# instance_type = "t2.micro"
+# server_name   = "Apache Example Server"
